834d5ddae3daee9e4455fd3b859e46ff60b899cb
Elias Segura <148815735+eliassegura100@users.noreply.github.com>
2025-01-21 18:01:29 -0800

    Add files via upload

---
+++ Elias/Elias/Elias/EliasApp.swift
@@ -0,0 +1,17 @@
+//
+//  EliasApp.swift
+//  Elias
+//
+//  Created by keckuser on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct EliasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
